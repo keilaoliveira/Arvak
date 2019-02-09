0b7cd9def6659a7a79911bee788dee9d7797ec27
Keila Oliveira <keila.oliver08@gmail.com>
2019-02-09 14:53:37 -0200

    Projeto Criado

---
+++ src/main/java/br/com/arvak/ArvakApplication.java
@@ -0,0 +1,14 @@
+package br.com.arvak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArvakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArvakApplication.class, args);
+	}
+
+}
+
