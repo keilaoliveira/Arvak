ef4473a9cc0a6ec6e1fe69d90b4234b31f8b52a3
Keila Oliveira <keila.oliver08@gmail.com>
2019-02-20 19:02:09 -0300

    Criação de Instanciação de Objetos (Dados Banco)

--- src/main/java/br/com/arvak/ArvakApplication.java
+++ src/main/java/br/com/arvak/ArvakApplication.java
@@ -1,14 +1,35 @@
 package br.com.arvak;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ArvakApplication {
+import br.com.arvak.domain.Grupo;
+import br.com.arvak.repositories.GrupoRepository;
 
+@SpringBootApplication
+public class ArvakApplication implements CommandLineRunner {
+
+	@Autowired
+	private GrupoRepository grupoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ArvakApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Grupo gp1 = new Grupo (null, "Informática");
+		Grupo gp2 = new Grupo (null, "Telefonia"); 
+		
+		grupoRepository.save(Arrays.asList(gp1, gp2));
+		
+	}
+
+	
 }
 
