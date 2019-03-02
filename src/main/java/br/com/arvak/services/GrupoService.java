24f11c0b849c4da7ab07a0f0035308f5a410164f
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-02 14:25:16 -0300

    Deletando um Grupo com DELETE

--- src/main/java/br/com/arvak/services/GrupoService.java
+++ src/main/java/br/com/arvak/services/GrupoService.java
@@ -1,10 +1,12 @@
 package br.com.arvak.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.arvak.domain.Grupo;
 import br.com.arvak.repositories.GrupoRepository;
+import br.com.arvak.services.exceptions.DataIntegrityException;
 import br.com.arvak.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -12,26 +14,36 @@ public class GrupoService {
 
 	@Autowired
 	private GrupoRepository repo;
-	
-	public Grupo find (Integer id) {
-		
+
+	public Grupo find(Integer id) {
+
 		Grupo obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Grupo não encontrado!!! Id: " + id + "!! Objeto do tipo: " + Grupo.class.getName());
+			throw new ObjectNotFoundException(
+					"Grupo não encontrado!!! Id: " + id + "!! Objeto do tipo: " + Grupo.class.getName());
 		}
 		return obj;
-		
+
 	}
-	
+
 	public Grupo insert(Grupo obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Grupo update(Grupo obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir um grupo que possui produtos vinculados!");
+		}
+	}
+
 }
