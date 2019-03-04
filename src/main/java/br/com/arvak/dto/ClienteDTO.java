a08b753f6147b863810f23d0e8195f5825b6d379
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-04 20:47:32 -0300

    Validação de atualização de email customizada no cliente

--- src/main/java/br/com/arvak/dto/ClienteDTO.java
+++ src/main/java/br/com/arvak/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import br.com.arvak.domain.Cliente;
+import br.com.arvak.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
