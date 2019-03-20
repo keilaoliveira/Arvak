8effef439dce51f98510a6472ec6befe4009367e
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-20 20:18:09 -0300

    Configurando S3 Amazon

--- src/main/java/br/com/arvak/ArvakApplication.java
+++ src/main/java/br/com/arvak/ArvakApplication.java
@@ -1,11 +1,17 @@
 package br.com.arvak;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.arvak.services.S3Service;
+
 @SpringBootApplication
 public class ArvakApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ArvakApplication.class, args);
@@ -13,7 +19,7 @@ public class ArvakApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {			
-		
+		s3Service.uploadFile("D:\\temp\\fotos\\notebookDell.png");
 	}
 
 	
