d411ef8f18363b6516780405601608a088f81da0
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-20 21:45:49 -0300

    Enviado imagem por endpoint

--- src/main/java/br/com/arvak/ArvakApplication.java
+++ src/main/java/br/com/arvak/ArvakApplication.java
@@ -1,17 +1,15 @@
 package br.com.arvak;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.arvak.services.S3Service;
-
 @SpringBootApplication
 public class ArvakApplication implements CommandLineRunner {
-	
+	/*
 	@Autowired
 	private S3Service s3Service;
+	*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ArvakApplication.class, args);
@@ -19,7 +17,7 @@ public class ArvakApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {			
-		s3Service.uploadFile("D:\\temp\\fotos\\notebookDell.png");
+		//s3Service.uploadFile("D:\\temp\\fotos\\notebookDell.png");
 	}
 
 	
--- src/main/java/br/com/arvak/services/ClienteService.java
+++ src/main/java/br/com/arvak/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.arvak.services;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.arvak.domain.Cidade;
 import br.com.arvak.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find (Integer id) {
 		
@@ -107,6 +112,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNomeCliente(obj.getNomeCliente());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
