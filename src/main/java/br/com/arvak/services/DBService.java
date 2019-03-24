c4e1621729763e86eb7fee205a9511ac993fc14d
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-24 00:53:00 -0300

    Liberação do Cors para as requisições put e delete / Inserção de produtos

--- src/main/java/br/com/arvak/services/DBService.java
+++ src/main/java/br/com/arvak/services/DBService.java
@@ -78,6 +78,90 @@ public class DBService {
 		Produto p10 = new Produto(null, "Kit Musculação 2 Barras Halteres", 250.00);
 		Produto p11 = new Produto(null, "Perfume Angel - Thierry Mugler - 100ml", 779.00);
 		
+		Produto p12 = new Produto(null, "Produto 12", 10.00);
+		Produto p13 = new Produto(null, "Produto 13", 10.00);
+		Produto p14 = new Produto(null, "Produto 14", 10.00);
+		Produto p15 = new Produto(null, "Produto 15", 10.00);
+		Produto p16 = new Produto(null, "Produto 16", 10.00);
+		Produto p17 = new Produto(null, "Produto 17", 10.00);
+		Produto p18 = new Produto(null, "Produto 18", 10.00);
+		Produto p19 = new Produto(null, "Produto 19", 10.00);
+		Produto p20 = new Produto(null, "Produto 20", 10.00);
+		Produto p21 = new Produto(null, "Produto 21", 10.00);
+		Produto p22 = new Produto(null, "Produto 22", 10.00);
+		Produto p23 = new Produto(null, "Produto 23", 10.00);
+		Produto p24 = new Produto(null, "Produto 24", 10.00);
+		Produto p25 = new Produto(null, "Produto 25", 10.00);
+		Produto p26 = new Produto(null, "Produto 26", 10.00);
+		Produto p27 = new Produto(null, "Produto 27", 10.00);
+		Produto p28 = new Produto(null, "Produto 28", 10.00);
+		Produto p29 = new Produto(null, "Produto 29", 10.00);
+		Produto p30 = new Produto(null, "Produto 30", 10.00);
+		Produto p31 = new Produto(null, "Produto 31", 10.00);
+		Produto p32 = new Produto(null, "Produto 32", 10.00);
+		Produto p33 = new Produto(null, "Produto 33", 10.00);
+		Produto p34 = new Produto(null, "Produto 34", 10.00);
+		Produto p35 = new Produto(null, "Produto 35", 10.00);
+		Produto p36 = new Produto(null, "Produto 36", 10.00);
+		Produto p37 = new Produto(null, "Produto 37", 10.00);
+		Produto p38 = new Produto(null, "Produto 38", 10.00);
+		Produto p39 = new Produto(null, "Produto 39", 10.00);
+		Produto p40 = new Produto(null, "Produto 40", 10.00);
+		Produto p41 = new Produto(null, "Produto 41", 10.00);
+		Produto p42 = new Produto(null, "Produto 42", 10.00);
+		Produto p43 = new Produto(null, "Produto 43", 10.00);
+		Produto p44 = new Produto(null, "Produto 44", 10.00);
+		Produto p45 = new Produto(null, "Produto 45", 10.00);
+		Produto p46 = new Produto(null, "Produto 46", 10.00);
+		Produto p47 = new Produto(null, "Produto 47", 10.00);
+		Produto p48 = new Produto(null, "Produto 48", 10.00);
+		Produto p49 = new Produto(null, "Produto 49", 10.00);
+		Produto p50 = new Produto(null, "Produto 50", 10.00);
+		
+		gp1.getProdutos().addAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
+		p12.getGrupos().add(gp1);
+		p13.getGrupos().add(gp1);
+		p14.getGrupos().add(gp1);
+		p15.getGrupos().add(gp1);
+		p16.getGrupos().add(gp1);
+		p17.getGrupos().add(gp1);
+		p18.getGrupos().add(gp1);
+		p19.getGrupos().add(gp1);
+		p20.getGrupos().add(gp1);
+		p21.getGrupos().add(gp1);
+		p22.getGrupos().add(gp1);
+		p23.getGrupos().add(gp1);
+		p24.getGrupos().add(gp1);
+		p25.getGrupos().add(gp1);
+		p26.getGrupos().add(gp1);
+		p27.getGrupos().add(gp1);
+		p28.getGrupos().add(gp1);
+		p29.getGrupos().add(gp1);
+		p30.getGrupos().add(gp1);
+		p31.getGrupos().add(gp1);
+		p32.getGrupos().add(gp1);
+		p33.getGrupos().add(gp1);
+		p34.getGrupos().add(gp1);
+		p35.getGrupos().add(gp1);
+		p36.getGrupos().add(gp1);
+		p37.getGrupos().add(gp1);
+		p38.getGrupos().add(gp1);
+		p39.getGrupos().add(gp1);
+		p40.getGrupos().add(gp1);
+		p41.getGrupos().add(gp1);
+		p42.getGrupos().add(gp1);
+		p43.getGrupos().add(gp1);
+		p44.getGrupos().add(gp1);
+		p45.getGrupos().add(gp1);
+		p46.getGrupos().add(gp1);
+		p47.getGrupos().add(gp1);
+		p48.getGrupos().add(gp1);
+		p49.getGrupos().add(gp1);
+		p50.getGrupos().add(gp1);
+		
 		gp1.getProdutos().addAll(Arrays.asList(p1, p2));
 		gp2.getProdutos().addAll(Arrays.asList(p3));
 		gp3.getProdutos().addAll(Arrays.asList(p4, p5));
@@ -142,6 +226,10 @@ public class DBService {
 		
 		grupoRepository.save(Arrays.asList(gp1, gp2, gp3, gp4, gp5, gp6, gp7));
 		produtoRepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+		
+		produtoRepository.save(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+				p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+				p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
 		
 		Estado est1 = new Estado(null, "São Paulo");
 		Estado est2 = new Estado(null, "Santa Catarina");
